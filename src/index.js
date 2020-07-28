9db1d9879826e07c96ce1213914e3fe7609ad697
Lucas Gabriel <lucasgabrielsa@hotmail.com>
2020-07-28 15:16:51 -0300

    Second class

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import CadastroVideo from './pages/cadastro/Video';
+import Home from './pages/Home/Home';
+import CadastroCategoria from './pages/cadastro/Categoria';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/cadastro/video" component={CadastroVideo} exact />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} exact />
+      <Route path="/" component={Home} exact />
+      <Route component={() => (<div>Página 404</div>)} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
