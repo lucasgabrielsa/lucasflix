0c0ebc0975ea768ab6ddf9aab81a7b33ff424485
Lucas Gabriel <lucasgabrielsa@hotmail.com>
2020-07-28 17:16:56 -0300

    CSS Category Page

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -5,12 +5,35 @@ import { Link } from 'react-router-dom';
 function CadastroCategoria() {
     return (
         <PageDefault>
-            <h1>Cadastro de Categoria</h1>
+            <div style={{ flexDirection: "row", textAlign: "center", fontSize: 35 }}>Nova Categoria</div>
 
+            <form style={{ flexDirection: "row", textAlign: "center" }}>
+                <label>
+                    Titulo:
+                    <input type="text" name="titulo" placeholder="Titulo" />
+                </label>
+
+                <label>
+                    Cor:
+                    <input type="text" name="cor" placeholder="Cor" />
+                </label>
+
+                <label>
+                    Descrição:
+                    <textarea type="text" name="descricao" placeholder="descricao" cols="4" rows="5"></textarea>
+                </label>
+
+                <label>
+                    Usuário:
+                    <input type="text" name="usuario" placeholder="Usuário" />
+                </label>
+            </form>
+
+            <br />
             <Link to="/">
                 Ir para o Home
             </Link>
-        </PageDefault>
+        </PageDefault >
     );
 }
 
