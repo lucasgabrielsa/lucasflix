ddb3ccf380737c83edd79259e85ee61ca215b1b7
Lucas Gabriel <lucasgabrielsa@hotmail.com>
2020-07-30 12:12:42 -0300

    Adding prop-types lib

--- src/components/FormField/index.js
+++ src/components/FormField/index.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const FormField = ({ value, name, onChange, label, type }) => {
-    return (
-        <div>
-            <label htmlFor="nome">{label}:</label>
-            <input type={type} name={name} id={name} value={value} onChange={onChange} />
-        </div>
-    );
-}
+const FormField = ({
+  value, name, onChange, label, type,
+}) => (
+  <div>
+    <label htmlFor="nome">
+      {label}
+      :
+    </label>
+    <input type={type} name={name} id={name} value={value} onChange={onChange} />
+  </div>
+);
+
+FormField.defaultProps = {
+  type: 'text',
+  value: '',
+};
+
+FormField.propTypes = {
+  label: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  type: PropTypes.string,
+  value: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+};
 
 export default FormField;
